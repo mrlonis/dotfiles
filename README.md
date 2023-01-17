7d717aaaf1b0c4b6c955ed72df4a8ea69ba47188
Matthew Lonis <mattlonis@gmail.com>
2023-01-17 10:45:19 -0500

    Fixing bugs in script

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotfiles
+
+This repository synchronizes dotfiles across machines.
+
+## Running
+
+To run the script, execute the following command:
+
+```shell
+python3 util.py
+```
--- util.py
+++ util.py
@@ -10,17 +10,17 @@ HOME = os.getenv("HOME")
 assert HOME is not None
 
 FILES = [
-    f"{HOME}/fun.py",
-    f"{HOME}/setup_all.sh",
-    f"{HOME}/requirements.in",
-    f"{HOME}/requirements.txt",
-    f"{HOME}/.bashrc",
-    f"{HOME}/.gitconfig",
-    f"{HOME}/.netrc",
-    f"{HOME}/.npmrc",
-    f"{HOME}/.profile",
-    f"{HOME}/.pypirc",
-    f"{HOME}/.zshrc",
+    "fun.py",
+    "setup_all.sh",
+    "requirements.in",
+    "requirements.txt",
+    ".bashrc",
+    ".gitconfig",
+    ".netrc",
+    ".npmrc",
+    ".profile",
+    ".pypirc",
+    ".zshrc",
 ]
 
 DESTINATION = Path("./files").resolve()
@@ -35,17 +35,42 @@ def copy_dot_files(folder: Union[str, Path], files: list[str], log: bool = False
         print(f"Folder exists: {folder}")
 
         for file in files:
-            file_path = Path(file).resolve()
-
-            if os.path.exists(file_path):
+            source_file_path = Path(os.path.join(HOME, os.path.basename(file))).absolute()
+            if os.path.exists(source_file_path) and not os.path.islink(source_file_path):
+                destination_file_path = Path(os.path.join(folder, os.path.basename(file))).resolve()
                 if log:
                     print(f"Copying file: {file}")
-                file_destination = os.path.join(folder, os.path.basename(file))
-                if os.path.exists(file_destination):
+                if os.path.exists(destination_file_path):
                     if log:
-                        print(f"Removing file: {file_destination}")
-                    os.remove(file_destination)
-                shutil.copy(Path(file).resolve(), file_destination)
+                        print(f"Removing file: {destination_file_path}")
+                    os.remove(destination_file_path)
+                shutil.copy(source_file_path, destination_file_path)
+
+
+def create_symlinks(folder: Union[str, Path], files: list[str], log: bool = False):
+    if log:
+        print(f"Creating symlinks in folder: {folder}")
+
+    for file in files:
+        src_file_path = Path(os.path.join(Path("./files").resolve(), os.path.basename(file))).resolve()
+
+        # Check if file exists in ./files
+        if os.path.exists(src_file_path):
+            destination_file_path = Path(os.path.join(folder, os.path.basename(file))).absolute()
+            print(f"Destination file path: {destination_file_path}")
+
+            # Check if file exists in folder directory
+            if os.path.exists(destination_file_path) or os.path.islink(destination_file_path):
+                if log:
+                    print(f"Removing file: {destination_file_path}")
+                os.remove(destination_file_path)
+
+            # Create symlink in folder directory
+            if log:
+                print(f"Creating symlink: {os.path.basename(file)}")
+            os.symlink(src_file_path, destination_file_path)
 
 
 copy_dot_files(DESTINATION, FILES, LOG)
+print("")
+create_symlinks(HOME, FILES, LOG)
