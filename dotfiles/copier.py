8a10dc857ef365db0e44b0a323f4e0e63d854b31
Matthew Lonis <mattlonis@gmail.com>
2023-02-08 11:50:33 -0500

    Updating copier

--- dotfiles/copier.py
+++ dotfiles/copier.py
@@ -18,7 +18,7 @@ def copy_dot_files(
             print(f"Copying files to folder: {destination_folder}")
 
         for file in filenames:
-            _copy_dot_file(
+            _process_file(
                 source_folder=source_folder,
                 destination_folder=destination_folder,
                 file=file,
@@ -31,7 +31,7 @@ def copy_dot_files(
             print(f"Destination folder {destination_folder} does not exist")
 
 
-def _copy_dot_file(
+def _process_file(
     source_folder: str | Path,
     destination_folder: str | Path,
     file: FileInput,
@@ -39,7 +39,6 @@ def _copy_dot_file(
     log: bool = False,
     test: bool = False,
 ):
-    # pylint: disable=too-many-branches
     if log:
         print("")
         print(f"Processing File: {file}")
@@ -57,32 +56,15 @@ def _copy_dot_file(
 
         if os.path.exists(destination_file_path):
             if overwrite:
-                if log:
-                    print(f"Removing file: {destination_file_path}")
-                if not test:
-                    os.remove(destination_file_path)
-                else:
-                    print("TEST: Not removing file")
-                if log:
-                    print(f"Copying {source_file_path} to {destination_file_path}")
-                if not test:
-                    shutil.copy(source_file_path, destination_file_path)
-                    if log:
-                        print(f"Successfully copied {source_file_path} to {destination_file_path}")
-                else:
-                    print("TEST: Not copying file")
+                _remove_file(destination_file_path=destination_file_path, log=log, test=test)
+                _copy_file(
+                    source_file_path=source_file_path, destination_file_path=destination_file_path, log=log, test=test
+                )
             else:
                 if log:
                     print(f"{file} exists and overwrite is False")
         else:
-            if log:
-                print(f"Copying {source_file_path} to {destination_file_path}")
-            if not test:
-                shutil.copy(source_file_path, destination_file_path)
-                if log:
-                    print(f"Successfully copied {source_file_path} to {destination_file_path}")
-            else:
-                print("TEST: Not copying file")
+            _copy_file(source_file_path=source_file_path, destination_file_path=destination_file_path, log=log, test=test)
     else:
         if log:
             if not exists:
@@ -91,3 +73,23 @@ def _copy_dot_file(
                 print(f"{source_file_path} is a symlink")
             else:
                 print(f"{source_file_path} had processing errors. exists: {exists}, is_symlink: {is_symlink}")
+
+
+def _remove_file(destination_file_path: str | Path, log: bool = False, test: bool = False):
+    if log:
+        print(f"Removing file: {destination_file_path}")
+    if not test:
+        os.remove(destination_file_path)
+    else:
+        print("TEST: Not removing file")
+
+
+def _copy_file(source_file_path: str | Path, destination_file_path: str | Path, log: bool = False, test: bool = False):
+    if log:
+        print(f"Copying {source_file_path} to {destination_file_path}")
+    if not test:
+        shutil.copy(source_file_path, destination_file_path)
+        if log:
+            print(f"Successfully copied {source_file_path} to {destination_file_path}")
+    else:
+        print("TEST: Not copying file")
