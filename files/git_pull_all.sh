800ef348c17b79cdf12ebfdfc7c870d5bc3cf6d4
Matthew Lonis <mattlonis@gmail.com>
2023-02-28 21:03:12 -0500

    Updating project

--- files/git_pull_all.sh
+++ files/git_pull_all.sh
@@ -1,12 +1,13 @@
-for org_dir in $HOME/Documents/GitHub/*; do
-    echo "Processing GitHub Organization: $org_dir"
-    cd "$org_dir"
+#!/bin/bash
+for org_dir in "$HOME"/Documents/GitHub/*; do
+	echo "Processing GitHub Organization: $org_dir"
+	cd "$org_dir" || return
 
-    for project_dir in $org_dir/*; do
-        if [ -d "$project_dir" ]; then
-            echo "Processing Project: $project_dir"
-            cd "$project_dir"
-            git pull
-        fi
-    done
+	for project_dir in "$org_dir"/*; do
+		if [ -d "$project_dir" ]; then
+			echo "Processing Project: $project_dir"
+			cd "$project_dir" || return
+			git pull
+		fi
+	done
 done
