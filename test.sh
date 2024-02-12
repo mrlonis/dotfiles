44788127065c3620cd930d99008e00f80d3b3d7f
Matthew Lonis <mattlonis@gmail.com>
2024-02-11 23:42:22 -0800

    Fix github actions

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 poetry run flake8 dotfiles tests util.py
 poetry run pylint dotfiles tests util.py
-poetry run pydocstyle dotfiles tests util.py
-poetry run bandit -c pyproject.toml -r .
 poetry run pytest --cov --cov-report=html -n auto
