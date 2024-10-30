aed451ce9cb230a3457fcdf32db4e33356338261
Matthew Lonis <mattlonis@gmail.com>
2024-10-29 23:02:34 -0400

    Updating dependencies

--- files/git_pull_all.sh
+++ files/git_pull_all.sh
@@ -7,6 +7,14 @@ for org_dir in "$HOME"/GitHub/*; do
 		if [ -d "$project_dir" ]; then
 			echo "Processing Project: $project_dir"
 			cd "$project_dir" || return
+			default_branch=$(git remote show origin | sed -n '/HEAD branch/s/.*: //p')
+			current_branch=$(git rev-parse --abbrev-ref HEAD)
+			echo "Default branch: $default_branch"
+			echo "Current branch: $current_branch"
+			# Switch back to default branch if not already on it
+			if [ "$current_branch" != "$default_branch" ]; then
+				git checkout "$default_branch"
+			fi
 			git pull
 			if [ -e .gitmodules ]; then
 				echo "Processing submodules for Project: $project_dir"
@@ -16,6 +24,9 @@ for org_dir in "$HOME"/GitHub/*; do
 			for branch in $(git branch -vv | grep ': gone]' | awk '{print $1}'); do
 				git branch -D "$branch"
 			done
+			echo "-"
+			default_branch=""
+			current_branch=""
 		fi
 	done
 done
